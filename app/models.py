db65d89a8b19ba1f5b3894e5fc3bbb9c9d80fdf1
[Michal Pavlov] <[pavlov@stud.uniza.sk]>
2025-01-10 15:13:37 +0100

    graficka uprava movie people

--- app/models.py
+++ app/models.py
@@ -34,7 +34,7 @@ class Movie(models.Model):
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.FloatField()
+    rating = models.IntegerField()
     comment = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
